e9691c9232480d54f9558b6f32e51874b8f7a5f8
Abdullah <dev.abdullllah@gmail.com>
2024-01-19 22:30:29 +0600

    Clear Home Controller and view

--- src/StackOverflow.Web/Controllers/HomeController.cs
+++ src/StackOverflow.Web/Controllers/HomeController.cs
@@ -1,34 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using StackOverflow.Infrastructure.UnitOfWorks;
-using StackOverflow.Web.Models;
-using System.Diagnostics;
 
 namespace StackOverflow.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly IUnitOfWork _unitOfWork;
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _unitOfWork = unitOfWork;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
