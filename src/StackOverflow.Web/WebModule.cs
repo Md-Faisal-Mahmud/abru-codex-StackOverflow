f39b8d9668697e524805874ac3d32c3548948d75
Abdullah <dev.abdullllah@gmail.com>
2024-01-19 22:21:56 +0600

    Refactor Tag Feature

--- src/StackOverflow.Web/WebModule.cs
+++ src/StackOverflow.Web/WebModule.cs
@@ -18,12 +18,11 @@ namespace StackOverflow.Web
             builder.RegisterType<LoginModel>().AsSelf()
                 .InstancePerLifetimeScope();
 
+            builder.RegisterType<TagModel>().AsSelf();
+
             builder.RegisterType<AddPostModel>().AsSelf();
             builder.RegisterType<PostListModel>().AsSelf();
             builder.RegisterType<DeletePostModel>().AsSelf();
-            builder.RegisterType<AddTagModel>().AsSelf();
-            builder.RegisterType<GetTagModel>().AsSelf();
-            builder.RegisterType<DeleteTagModel>().AsSelf();
             builder.RegisterType<UpdatePostModel>().AsSelf();
             builder.RegisterType<DeleteAnswerModel>().AsSelf();
             builder.RegisterType<UpdateAnswerModel>().AsSelf();
